684d566befdcf2107013d957639f78fc4b147504
yarkob <yaroslav.klark.bondarenko@gmail.com>
2024-02-11 12:23:10 +0200

    Solution v2

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,8 +1,11 @@
 // Add the required props
 import { UserInfo } from '../UserInfo';
+import cn from 'classnames';
 
 export const TodoInfo = ({ todo }) => (
-  <article className="TodoInfo TodoInfo--completed">
+  <article className={cn('TodoInfo', {
+    'TodoInfo--completed': todo.completed
+  })}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
     <UserInfo user={todo.user} />
